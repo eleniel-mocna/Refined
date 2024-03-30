6b54ae46090518ea750d057aff8817aa6c45407e
Samuel Soukup <samuel.soukup@jetbrains.com>
2024-03-30 13:00:12 +0100

    Refactor RFCSurrounding class to big_rfc.py and remove old file.

--- src/models/big_rfc/big_rfc.py
+++ src/models/big_rfc/big_rfc.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 
 from config.config import Config
-from models.big_rfc.RFCSurrounding import RFCSurrounding
+from models.common.ProteinModel import SurroundingsProteinModel
 from models.cutoff import get_best_cutoff
 from models.evaluation.ModelEvaluator import ModelEvaluator, booleanize
 
@@ -13,49 +13,42 @@ import pickle
 from sklearn.model_selection import train_test_split
 
 
-def generate_BigRFC_model(data: np.ndarray, labels: np.ndarray) -> RFCSurrounding:
-    """
-    Create a GeneralModel implementation using a RandomForestClassifier.
-    @param data: Numpy array of atom neighborhoods
-    @param labels: Numpy array of labels for each atom
-    @return: trained RandomForestModel
-    """
-    train_data, test_data, train_labels, test_labels = \
-        train_test_split(data, labels, test_size=0.20, random_state=42)
-    y_train = train_labels
-    y_test = test_labels
+class RFCSurrounding(SurroundingsProteinModel):
+    def predict_surroundings(self, protein: np.ndarray) -> np.ndarray:
+        return self.rfc.predict_proba(protein)[:, 1] > self.cutoff
 
-    X_train = train_data
-    X_test = test_data
-    print("Data prepared, training RFC...")
-    random_forest: RandomForestClassifier = RandomForestClassifier(max_depth=5, n_jobs=15, verbose=3, n_estimators=15)
-    random_forest.fit(X_train, y_train)
-    print("RFC trained, finding best cutoff...")
-    best_cutoff = get_best_cutoff(X_test, y_test, random_forest)
-    print(best_cutoff)
-    return RFCSurrounding(random_forest, best_cutoff)
+    def predict_surroundings_proba(self, protein: np.ndarray) -> np.ndarray:
+        return self.rfc.predict_proba(protein)
 
+    @property
+    def name(self) -> str:
+        return "rfc_surrounding"
 
-def save_BigRFC_model(model: RFCSurrounding, file: str):
-    """
-    Save a RandomForestModel to a pickle file.
-    @param model: RandomForestModel
-    @param file: pickle file
-    """
-    with open(file, "wb") as file:
-        pickle.dump(model, file)
+    def __init__(self, rfc: RandomForestClassifier, cutoff: float):
+        self.cutoff: float = cutoff
+        self.rfc: RandomForestClassifier = rfc
 
+    @staticmethod
+    def from_data(data: np.ndarray, labels: np.ndarray):
+        """
+        Create a GeneralModel implementation using a RandomForestClassifier.
+        @param data: Numpy array of atom neighborhoods
+        @param labels: Numpy array of labels for each atom
+        @return: trained RandomForestModel
+        """
+        train_data, test_data, train_labels, test_labels = \
+            train_test_split(data, labels, test_size=0.20, random_state=42)
 
-def load_BigRFC_model(file: str) -> RFCSurrounding:
-    """
-    Load a RandomForestModel from a pickle file.
-    @param file: pickle file
-    @return: RandomForestModel
-    """
-    with open(file, "rb") as file:
-        random_forest = pickle.load(file)
-    return random_forest
-
+        train_data = train_data
+        test_data = test_data
+        print("Data prepared, training RFC...")
+        random_forest: RandomForestClassifier = RandomForestClassifier(max_depth=5, n_jobs=15, verbose=3,
+                                                                       n_estimators=15)
+        random_forest.fit(train_data, train_labels)
+        print("RFC trained, finding best cutoff...")
+        best_cutoff = get_best_cutoff(test_data, test_labels, random_forest)
+        print(best_cutoff)
+        return RFCSurrounding(random_forest, best_cutoff)
 
 def main():
     config = Config.get_instance()
@@ -63,7 +56,7 @@ def main():
         data, labels = pickle.load(file)
     labels = np.vectorize(booleanize)(labels)
 
-    rfc_surrounding_model = generate_BigRFC_model(np.array(data), np.array(labels))
+    rfc_surrounding_model = RFCSurrounding.from_data(np.array(data), np.array(labels))
     rfc_surrounding_model.save_model()
     (ModelEvaluator(rfc_surrounding_model)
      .calculate_basic_metrics()
