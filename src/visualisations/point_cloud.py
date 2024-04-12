f9c44be9b21a8baeb41b1e1941c05fc0806cc50c
Samuel Soukup <samuel.soukup@jetbrains.com>
2024-04-12 10:47:45 +0200

    Add a point cloud visualiser for the thesis creation

---
+++ src/visualisations/point_cloud.py
@@ -0,0 +1,44 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+
+from config.constants import DATA_FOLDER, IMAGES_FOLDER
+from dataset.extract_data.extract_arffs import _load_arffs
+
+
+def do_the_plot(x, y, z, c, name: str):
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+    ax.scatter3D(-x, y, z, c=c, alpha=0.4, s=4, marker='.')
+    ax.view_init(190, -10, 0)
+    IMAGES_FOLDER.mkdir(exist_ok=True)
+    plt.savefig(IMAGES_FOLDER / name, dpi=1200, bbox_inches='tight')
+    # plt.show()
+
+
+if __name__ == '__main__':
+    path_1fbl = DATA_FOLDER / "raw/joined/b210/1fbl"
+    df = _load_arffs(path_1fbl)[0]
+    df["@@class@@"] = df["@@class@@"] == b'1'
+
+    do_the_plot(df["xyz.x"],
+                df["xyz.y"],
+                df["xyz.z"],
+                ["red" if x else "green" for x in df["@@class@@"]],
+                "point_cloud_class.png")
+    pca = PCA(3)
+    features = df.drop(["xyz.x", "xyz.y", "xyz.z", "@@class@@"], axis=1).to_numpy()
+    print(f"Features shape: {features.shape}")
+    c=pca.fit_transform(features)
+
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    c = scaler.fit_transform(c)
+    do_the_plot(df["xyz.x"],
+                df["xyz.y"],
+                df["xyz.z"],
+                c,
+                "point_cloud_pca.png")
+
+
+
