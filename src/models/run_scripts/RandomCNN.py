f5a6f8f69768d919e915b53e62666f188cc74c5b
Samuel Soukup <samuel.soukup@jetbrains.com>
2024-04-18 12:40:01 +0200

    Add best hyperparams to baseline CNNs.

--- src/models/run_scripts/RandomCNN.py
+++ src/models/run_scripts/RandomCNN.py
@@ -10,6 +10,16 @@ from models.refined.RefinedModel import generate_refined_model
 np.set_printoptions(threshold=sys.maxsize)
 import pickle
 
+best_hyperparams = {
+    "initial_size": 128,
+    "growth_factor": 0.95,
+    "last_dense": 32,
+    "cnn_layers": 1,
+    "cnn_stride": 2,
+    "kernel_size": 3,
+    "learning_rate": 5e-05
+}
+
 
 def main():
     with open(config.train_surroundings, "rb") as file:
@@ -17,7 +27,7 @@ def main():
 
     refined = RandomRefined(38, 30)
 
-    rfc_surrounding_model, _ = generate_refined_model(np.array(data), np.array(labels), refined)
+    rfc_surrounding_model, _ = generate_refined_model(np.array(data), np.array(labels), refined, best_hyperparams)
     rfc_surrounding_model.save_model()
     return (ModelEvaluator(rfc_surrounding_model)
             .calculate_basic_metrics()
--- src/models/run_scripts/RandomCNN_Normalized.py
+++ src/models/run_scripts/RandomCNN_Normalized.py
@@ -10,6 +10,15 @@ from models.refined.RefinedModel import generate_refined_model
 np.set_printoptions(threshold=sys.maxsize)
 import pickle
 
+best_params = {"initial_size": 128,
+               "growth_factor": 1.4,
+               "last_dense": 128,
+               "cnn_layers": 3,
+               "cnn_stride": 1,
+               "kernel_size": 3,
+               "learning_rate": 5e-05,
+               }
+
 
 def main():
     with open(config.train_surroundings, "rb") as file:
@@ -17,7 +26,7 @@ def main():
 
     refined = RandomRefinedNormalized(data, 38, 30)
 
-    rfc_surrounding_model, _ = generate_refined_model(np.array(data), np.array(labels), refined)
+    rfc_surrounding_model, _ = generate_refined_model(np.array(data), np.array(labels), refined, best_params)
     rfc_surrounding_model.save_model()
     return (ModelEvaluator(rfc_surrounding_model)
             .calculate_basic_metrics()
